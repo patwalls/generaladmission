ac4d0e8d3f4bb91016d8d51720534cc4acd660ce
Pat Walls <patrickswalls@gmail.com>
2016-03-06 16:34:31 -0800

    Fix CORS issue

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,3 +1,2 @@
 class Api::SearchController < ApplicationController
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_filter :verify_authenticity_token, :only => [:update]
   protect_from_forgery with: :exception
 
   # Expose these methods to the views
